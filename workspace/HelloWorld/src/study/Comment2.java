1eaf78670b8c93719530495ce4336be31a6b9b9b
wula50 <wula50@163.com>
2020-08-20 16:36:47 +0800

    0820

---
+++ workspace/HelloWorld/src/study/Comment2.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package study;
+
+/**
+ * @description：
+ * @author: Ada
+ * @package:study
+ * @date:   Aug 20, 20201:13:09 PM
+ */
+public class Comment2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
